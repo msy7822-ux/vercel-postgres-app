86f88acd6bf4fd5d89fac9f97f943060b34fbd1f
msy7822-ux <msy780022@gmail.com>
2023-05-02 18:31:23 +0900

    minor modification🗿

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,13 @@ import { Post } from "@/types/PostType";
 import { NextPage } from "next";
 
 export async function getStaticProps() {
-  console.log("env", process.env.NEXT_PUBLIC_BASE_URL);
-  // const res = await fetch(
-  //   `${process.env.NEXT_PUBLIC_BASE_URL}/api/db/fetchPosts`
-  // );
-  // const json = await res.json();
-  // const posts = json.data;
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/db/fetchPosts`
+  );
+  const json = await res.json();
+  const posts = json.data;
   return {
-    props: { posts: [] },
+    props: { posts },
     revalidate: 10,
   };
 }
