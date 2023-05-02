8ed8e9dcf6d558bef4d6312567d7b58d1637e375
msy7822-ux <msy780022@gmail.com>
2023-05-02 19:28:33 +0900

    minor modification🗿

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,15 @@ import { Post } from "@/types/PostType";
 import { NextPage } from "next";
 
 export const getStaticProps = async () => {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/db/fetchPosts`
-    );
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/db/fetchPosts`
+  );
 
-    const json = await res.json();
-    const posts = json.data;
-    return {
-      props: { posts },
-    };
-  } catch (err) {
-    console.log("err => ", err);
-    return {
-      props: { posts: [] },
-    };
-  }
+  const json = await res.json();
+  const posts = json.data;
+  return {
+    props: { posts },
+  };
 };
 
 const Home: NextPage<{ posts: Post[] }> = ({ posts }) => {
